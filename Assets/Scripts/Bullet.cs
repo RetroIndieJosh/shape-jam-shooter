d2b75c4ccb7edfe63766b793fb801e80eead98ab
Joshua McLean <=>
2020-01-17 10:19:59 -0500

    Limit bullet move distance

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -8,11 +8,13 @@ namespace EightBitsToInfinity {
     public class Bullet : MonoBehaviour
     {
         [SerializeField] private int m_damage = 1;
+        [SerializeField] private float m_moveLimit = 100f;
 
         public int Damage => m_damage;
 
         private AnimatedSpriteRenderer m_animator = null;
         private Rigidbody2D m_body = null;
+        private Vector2 m_origin = Vector2.zero;
 
         public void Collide() {
             m_body.velocity = Vector2.zero;
@@ -22,6 +24,11 @@ namespace EightBitsToInfinity {
         private void Awake() {
             m_animator = GetComponent<AnimatedSpriteRenderer>();
             m_body = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update() {
+            if (Vector2.Distance(transform.position, m_origin) > m_moveLimit)
+                Destroy(gameObject);
         }
 
         private void Start() {
@@ -42,6 +49,7 @@ namespace EightBitsToInfinity {
 
             // created bullet is moving automatically
             m_animator.SetAnimation("move");
+            m_origin = transform.position;
         }
     }
 }
